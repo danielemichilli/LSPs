4f4b4dfd6482805cbc314ef7dad515764cbe751b
Daniele Michilli <danielemichilli@gmail.com>
2015-12-14 16:53:55 +0100

    Candidates selection fixed

--- src/Candidates.py
+++ src/Candidates.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import multiprocessing as mp
 import logging
 
 import Pulses
@@ -10,16 +9,17 @@ import Utilities
 
 def candidates(pulses,idL):
   pulses.sort(['Pulse','Sigma'],ascending=[1,0],inplace=True)
-
-  pulses.Candidate[pulses.Pulse==0] = repeated_parallel(pulses[pulses.Pulse==0],0)
+  
+  pulses.Candidate[pulses.Pulse==0] = Repeated_candidates_beam(pulses[pulses.Pulse==0],0)
   
   if pulses.Candidate.unique().size <=12:
-    pulses.Candidate[(pulses.Pulse<=1)&(pulses.Candidate<0)] = repeated_parallel(pulses[(pulses.Pulse<=1)&(pulses.Candidate<0)],1)
+    pulses.Candidate[(pulses.Pulse<=1)&(pulses.Candidate<0)] = Repeated_candidates_beam(pulses[(pulses.Pulse<=1)&(pulses.Candidate<0)],1)
 
   if pulses.Candidate.unique().size <=12:
-    pulses.Candidate[pulses.Candidate<0] = repeated_parallel(pulses[pulses.Candidate<0],2)
-  
-  cands_unique = pulses[(pulses.Candidate==-1)&(pulses.Sigma>=10)].groupby(['SAP','BEAM'],sort=False)[['SAP','BEAM']].head(2).astype(np.int16)
+    pulses.Candidate[pulses.Candidate<0] = Repeated_candidates_beam(pulses[pulses.Candidate<0],2)
+
+  cands_unique = pulses[(pulses.Candidate==-1)&(pulses.Sigma>=10)].groupby(['SAP','BEAM'],sort=False)[['SAP','BEAM']].head(5).astype(np.int32)
+  #ATTENZIONE: aumentare head(2) e selezionare head(2) solo dopo aver visto quali non sono in altri candidates
   pulses.Candidate.loc[cands_unique.index.get_level_values('idx')] = np.arange(cands_unique.shape[0]) * 1000 + cands_unique.SAP * 100 + cands_unique.BEAM
   
   if not pulses[pulses.Candidate>=0].empty:
@@ -37,52 +37,37 @@ def candidates(pulses,idL):
   return cands
 
 
-def repeated_parallel(pulses,rank):
+def Repeated_candidates_beam(pulses,rank):
   gb_puls = pulses.groupby(['SAP','BEAM'],sort=False)
   dirs = [n for n in gb_puls.indices.iterkeys()]
-  
-  CPUs = mp.cpu_count()
-  dirs_range = int(np.ceil(len(dirs)/float(CPUs)))
+  pulses['cand'] = -1
 
-  pool = mp.Pool()
-  dirs_CPU = [dirs[i*dirs_range:(i+1)*dirs_range] for i in range(CPUs) if len(dirs[i*dirs_range:(i+1)*dirs_range]) > 0]
-  results = [pool.apply_async(Repeated_candidates_beam, args=(pulses[(pulses.Pulse==0)&pulses.SAP.isin(zip(*dir_CPU)[0])&pulses.BEAM.isin(zip(*dir_CPU)[1])],dir_CPU,rank)) for dir_CPU in dirs_CPU]
-  pool.close()
-  pool.join()
-  return pd.concat([p.get() for p in results])
-
-
-def Repeated_candidates_beam(pulses,dirs,rank):
-  result = pd.Series()
   for (sap,beam) in dirs:
-    pulses = pulses[(pulses.SAP==sap)&(pulses.BEAM==beam)].copy()
-    pulses.DM = 3*(pulses.DM.astype(np.float64)/3).round(2)
+    puls = pulses[(pulses.SAP==sap)&(pulses.BEAM==beam)].copy()
+    puls.DM = 3*(puls.DM.astype(np.float64)/3).round(2)
     
     span = 0.25
     
-    top_count = pulses.groupby('DM')['Sigma'].count()
-    top_sum = pulses.groupby('DM')['Sigma'].sum()
+    top_count = puls.groupby('DM')['Sigma'].count()
+    top_sum = puls.groupby('DM')['Sigma'].sum()
     
     top_sum = top_sum[top_count >= 2]
     #top_count = top_count[top_count >= 2]
 
-    cand = pulses.N_events.astype(np.int32)
-    cand[:] = -1
-    i = 10
+    i = 1
 
     while not top_sum[top_sum!=0].empty:
       DM = top_sum.argmax()
       #Sigma = top_sum.loc[DM-span:DM+span].sum()
-      #N_pulses = top_count.loc[DM-span:DM+span].sum()
-      if cand[(pulses.DM>=DM-span)&(pulses.DM<=DM+span)].shape[0] > 1:
-        cand[(pulses.DM>=DM-span)&(pulses.DM<=DM+span)] = (i+1) * 10000 + sap * 1000 + beam * 10 + rank
+      #N_puls = top_count.loc[DM-span:DM+span].sum()
+      selected_pulses = puls.cand[(puls.DM>=DM-span)&(puls.DM<=DM+span)]
+      if selected_pulses.shape[0] > 1:
+        pulses.cand.loc[selected_pulses.index] = i * 100000 + sap * 1000 + beam * 10 + rank
       #top_count.loc[DM-span:DM+span] = 0
       top_sum.loc[DM-span:DM+span] = 0
-      i += 1
-    
-    result = pd.concat((cand,result))
-    
-  return result
+      i += 1    
+
+  return pulses.cand
 
 
 def period(x):
