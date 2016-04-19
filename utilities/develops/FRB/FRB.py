392f18a1482d0e777bfb354ab37255aa0c5e95d6
Daniele Michilli <danielemichilli@gmail.com>
2016-04-19 17:36:50 +0200

    Grid for imaging

--- utilities/develops/FRB/FRB.py
+++ utilities/develops/FRB/FRB.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import glob
 import multiprocessing as mp
@@ -31,28 +30,18 @@ Number_of_candidates = 10
 
 
 def beam_matrix():
-  """
+  '''
   Load the timesieries for a beam and transform the signal in SNR
-  """
-  
-
+  '''
   #Forse possibile detrend in DM ogni time bin?
 
-  #Convert signal in SNR
-  pool = mp.Pool()
-  results = pool.map_async(time_fft, range(os.cpu_count()-1))
-  pool.close()
-  pool.join()
+  #Create and save the SNR datacube
+  time_fft_parallel()
   
-  beam = np.vstack(results)
-  results = 0
-  
-  np.save('../FRB_beam_matrix_SNR',beam)
-  beam = 0
-
+  #Create and save the signal datacube
   for idx,DM in enumerate(np.arange(0,500)):
     file = glob.glob('*_DM{}.dat'.format(DM))[0]
-    if idx ==0: 
+    if idx == 0: 
       beam0 = np.fromfile(file,dtype=np.float32)
       beam = np.zeros((500,beam0.size))
       beam[0] = beam0
@@ -64,16 +53,36 @@ def beam_matrix():
   return
   
   
+def time_fft_parallel():
+  '''
+  Parallelize the time_fft function
+  '''
+  CPUs = mp.cpu_count()
+  pool = mp.Pool(CPUs)
+  results = pool.map_async(time_fft, range(CPUs))
+  pool.close()
+  pool.join()
   
-def time_fft(CPU):
-  DM_range = 500 / (os.cpu_count()-1)
+  beam = np.vstack(results)
+  np.save('../FRB_beam_matrix_SNR',beam)
   
-  timeseries = np.zeros((DM_range,36000))
+  return
 
+
+def time_fft(CPU):
+  '''
+  Create the SNR datacube 
+  '''
+  CPUs = mp.cpu_count()
+  DM_range = int(np.ceil(500/float()))  #Number of DMs in each CPU
+  
+  if DM_range * CPU > 500: timeseries = np.zeros((500%CPUs,36000))
+  else: timeseries = np.zeros((DM_range,36000))
   downfacts = [2, 5, 10, 20, 50, 100, 200, 500, 1000]
   chunklen = timeseries.shape[1]
   
-  for idx,DM in enumerate(np.arange(CPU*DM_range,(CPU+1)*DM_range)):
+  #Produce the datacube
+  for idx,DM in enumerate(np.arange(CPU*DM_range,np.clip((CPU+1)*DM_range,0,500))):
     file = glob.glob('*_DM{}.dat'.format(DM))[0]
     ts = np.fromfile(file,dtype=np.float32)[:36000]
     
@@ -114,7 +123,7 @@ def FRBs_finder():
         592, 495, 399, 304, 207, 110, 107, 103, 101, 199, 299, 399, 502,
         602, 701, 800, 796, 792, 788, 785, 687, 590, 494, 399, 305, 209,
         112,  14,  11,   7,   3,   0,  98, 198, 298])
-  
+
   dec = np.array([400, 500, 450, 350, 300, 350, 450, 600, 550, 500, 400, 300, 250,
         200, 250, 300, 400, 500, 550, 700, 650, 600, 550, 450, 350, 250,
         200, 150, 100, 150, 200, 250, 350, 450, 550, 600, 650, 800, 750,
@@ -132,19 +141,18 @@ def FRBs_finder():
   
 
 
-##
+def space_fft_parallel()
   pool = mp.Pool()
-  results = pool.map(space_fft, range(os.cpu_count()-1))
+  results = pool.map(space_fft, range(mp.cpu_count()))
   pool.close()
   pool.join()
   
   beam = np.vstack(results)
-  results = 0
-
+  return beam
   
   
 def space_fft(CPU):
-  t_range = 36000 / (os.cpu_count()-1)
+  t_range = 36000 / (mp.cpu_count())
   
   beams = data[:,:,CPU*t_range:(CPU+1)*t_range]
   
@@ -164,11 +172,29 @@ def space_fft(CPU):
     conv = signal.convolve2d(beams_map,np.ones((fill,fill))/fill,mode='same')  #Check the statistics!
     
     
-    
     #Condizioni per salvare il bin
-    
-    
-    
+
+
     if cand_idx[cand_idx>=0].size == 0: break
   
   
+#Create the convolved matrix
+beams_map = np.zeros((9,9))
+kern1 =  = np.ones((2,3))
+kern2 =  = np.ones((3,2))
+conv1 = signal.convolve2d(beams_map,kern1,mode='full')
+conv2 = signal.convolve2d(beams_map,kern2,mode='full')
+
+conv1[1:-1,1] /= 2
+conv1[1:-1,-2] /= 2
+conv1[0,2:-2:2] /= 2
+conv1[-1,2:-2:2] /= 2
+conv1[1:-1,2:-2] /= 3
+
+conv2[1,1:-1] /= 2
+conv2[-2,1:-1] /= 2
+conv2[2:-2:2,0] /= 2
+conv2[2:-2:2,-1] /= 2
+conv2[2:-2,1:-1] /= 3
+
+conv = #Bisogna prendere il massimo per ogni beam
