4785bff42dcdec3d186c09603afa1065e544cc38
danielemichilli <danielemichilli@gmail.com>
2018-10-15 18:10:36 -0400

    Changes for Pipeline v2

--- src/Paths.py
+++ src/Paths.py
@@ -6,6 +6,7 @@ SITE_CERT = os.path.join(os.environ["LOTAAS_PL"], "LSP/.site_cert.json")
 WRK_FOLDER = '/dev/shm/wrk'
 TMP_FOLDER = '/dev/shm/tmp'
 RAW_TEMP = os.environ["LOTAAS_RAW2"]
-MODEL_FILE = os.path.join(os.environ["LOTAAS_PL"], "LSP/sp_ML.model")
 CLASSIFIER = os.path.join(os.environ["LOTAAS_PL"], "scores_robLyon/PulsarProcessingScripts-master/ML.jar") 
 
+#MODEL_FILE = os.path.join(os.environ["LOTAAS_PL"], "LSP/sp_ML.model")
+MODEL_FILE = "/lustre4/0/lotaas2/software4/LOTAAS-Scripts_v2/LSP/sp_ML.model"
